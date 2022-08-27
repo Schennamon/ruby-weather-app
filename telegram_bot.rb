3117b00d31114a78d387160e3fd271b458f85d02
itolokneev <itolokneev.codica@gmail.com>
2022-08-27 22:19:18 +0300

    Initial commit

---
+++ telegram_bot.rb
@@ -0,0 +1,35 @@
+require 'telegram/bot'
+require_relative 'weather'
+
+class TelegramBot
+  TOKEN = '5442705416:AAGA6XqEUEoFL_FEf-2qaw3G9owTOw6Qrgs'.freeze
+
+  def run
+    bot.listen do |message|
+      weather_message(message)
+    rescue => e
+      puts e.message
+    end
+  end
+
+  private
+
+  def bot
+    Telegram::Bot::Client.run(TOKEN) { |bot| return bot }
+  end
+
+  def weather_message(message)
+    return unless message.text.include? '/weather'
+   
+    send_message(message.chat.id, Weather.new(city_name(message.text)).form_message)
+  end
+
+  def city_name(text)
+    text.gsub('/weather', '').strip.tr(' ', '+')
+  end
+
+  def send_message(chat_id, message)
+    bot.api.sendMessage(chat_id: chat_id, text: message)
+  end
+  
+end
